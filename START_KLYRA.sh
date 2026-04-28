7d9e4ecb274e08fe564452a8f9c281ece75bd8a8
UnclePhilburt <cody2931@gmail.com>
2026-04-28 11:33:59 -0500

    Add auto-restart loop to START_KLYRA.sh

--- START_KLYRA.sh
+++ START_KLYRA.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-# Klyra Machine Launcher
+# Klyra Machine Launcher with Auto-Restart
 # Double-click this file to start Klyra
 
 cd "$(dirname "$0")/client"
 source venv/bin/activate
-python3 client_companion.py
+
+echo "Starting Klyra with auto-restart..."
+echo "Press Ctrl+C twice to stop completely"
+
+while true; do
+    python3 client_companion.py
+    echo ""
+    echo "Klyra crashed! Restarting in 3 seconds..."
+    sleep 3
+done
